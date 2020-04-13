5e18a5a81c32b497c43627427d0b0cbefcba71a3
esaur22 <esaur22@yahoo.com>
2020-04-13 09:31:51 -0600

    Agregando whitenoise a los middlewares 2

--- platinumWeb/settings.py
+++ platinumWeb/settings.py
@@ -55,11 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
+
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
